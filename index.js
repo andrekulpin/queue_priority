7c68f705fdf5fc8db1f0838dcdf60cf78f713547
andreykulpin <andreykulpin@onetwotrip.com>
2016-12-29 23:35:09 +0300

    getMax

--- index.js
+++ index.js
@@ -2,23 +2,40 @@ module.exports = PriorityHeap;
 
 function PriorityHeap( array, comparator ){
 	this.DEFAULT_COMPARATOR = this.DEFAULT_COMPARATOR || comparator;
-	this.data = array ? __buildHeap__( array ) : [];
+	this.data = array || [];
+	__buildHeap__( array );
 }
 
 PriorityHeap.prototype.DEFAULT_COMPARATOR = function(){}
 
 PriorityHeap.prototype.push = function( task ){
 	this.data.push( task );
+	__insert__(this.data, this.data.length - 1);
+}
+
+PriorityHeap.prototype.getMax = function( task ){
+	var max = this.data[0];
+	this.data = this.data.slice(1);
+	__buildHeap__(this.data);
+	return max;
 }
 
 function __buildHeap__( array ){
-	var l = array.length;
-	array = [ true ].concat(array)
+	var l = array.length - 1;
 	var i = 1
-	while( 0 < l ){
+	while( 0 <= l ){
 		__heapify__( array, l-- );
 	}
-	return array
+}
+
+function __insert__( heap, i ){
+	var p = Math.floor(i / 2);
+	if(heap[i] > heap[p]){
+		var tmp = heap[p];
+		heap[p] = heap[i];
+		heap[i] = tmp;
+		__insert__(heap, p);
+	}
 }
 
 function __heapify__( heap, i ){
--- test.js
+++ test.js
@@ -1,4 +1,12 @@
 const Queue = require('./index')
 
-var queue = new Queue([9,8,7,6,1,4,3,10])
+var queue = new Queue([9,8,7,6,1,4,3,10]);
+
+var L = 10;
+
+while(L--){
+	queue.push(Math.floor(Math.random() * 100));
+}
+
+console.log(queue.getMax())
 console.log(queue)
